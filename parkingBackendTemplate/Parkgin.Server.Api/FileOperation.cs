f22c7b80074e1ea4a20ffeb0cf1b0cdd5e7bf125
Ethan Hong <108930624+Ethan-SubGit@users.noreply.github.com>
2022-07-11 00:20:38 +0900

    Update FileOperation.cs

--- parkingBackendTemplate/Parkgin.Server.Api/FileOperation.cs
+++ parkingBackendTemplate/Parkgin.Server.Api/FileOperation.cs
@@ -14,32 +14,7 @@ namespace Parkgin.Server.Api
 
         public void Apply(OpenApiOperation operation, OperationFilterContext context)
         {
-            //if (operation.OperationId.ToLower() == "apifileuploadpost")
-            //{
-            //    operation.Parameters.Clear();//Clearing parameters
-            //    operation.Parameters.Add(new NonBodyParameter
-            //    {
-            //        Name = "File",
-            //        In = "formData",
-            //        Description = "Upload Image",
-            //        Required = true,
-            //        Type = "file"
-            //    });
-            //    operation.Consumes.Add("application/form-data");
-            //}
-            //if (operation.OperationId == "Post")
-            //{
-            //    operation.Parameters = new List<IParameter>
-            //    {
-            //        new NonBodyParameter
-            //        {
-            //            Name = "myFile",
-            //            Required = true,
-            //            Type = "file",
-            //            In = "formData"
-            //        }
-            //    };
-            //}
+           
         }
     }
 }
